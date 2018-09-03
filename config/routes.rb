b61528822e6fb5424cee0f25879ff862f1203fca
heyouli <julia@julia-hofmann.com>
2018-09-03 15:13:50 +0200

    5.1 add dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  get 'simple_pages/landing_page'
+  get 'products/index'
+  get 'products/new'
+  get 'products/edit'
+  get 'products/show'
+  get 'orders/index'
+  get 'orders/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#landing_page'
+  root 'products#index'
 end
