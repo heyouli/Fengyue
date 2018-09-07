43c64e65c231d9edd532b40b8b4f4bdd1dcd8fe5
heyouli <julia@julia-hofmann.com>
2018-09-07 19:45:24 +0200

    5.2 Add search box, add form styles

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   get 'orders/index'
   get 'orders/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
+  root 'simple_pages#landing_page'
 end
