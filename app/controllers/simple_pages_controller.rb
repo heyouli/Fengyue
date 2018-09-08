6e4c10a3fa4d6a563b4892b2ca1db3b512cf747c
heyouli <julia@julia-hofmann.com>
2018-09-08 09:40:47 +0200

    5.3 crate mailers

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,4 +3,14 @@ class SimplePagesController < ApplicationController
     @featured_product = Product.first
     @products = Product.all
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'julia@julia-hofmann.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'products/show'
   get 'orders/index'
   get 'orders/show'
+  post 'simple_pages/thank_you'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'simple_pages#landing_page'
 end
