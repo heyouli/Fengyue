78258297bb0e783d2e30c68d2c5d23a0a7e82de4
heyouli <julia@julia-hofmann.com>
2018-09-19 09:44:29 +0200

    5.7.Add comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,13 @@
 class Product < ApplicationRecord
   has_many :orders
-    def self.search(search_term)
-      if Rails.env.development?
-        Product.where("name LIKE ?", "%#{search_term}%")
-      else
-        Product.where("name ilike ?", "%#{search_term}%")
-      end
-    end
+  has_many :comments
+  def self.search(search_term)
+      Product.where("name LIKE ?", "%#{search_term}%")
   end
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
+end
