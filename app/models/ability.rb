01006831214b967234c3f8a3770d700add48bc0f
heyouli <julia@julia-hofmann.com>
2018-09-24 15:29:54 +0200

    5.10 edited cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,9 +6,9 @@ class Ability
     if user.admin?
       can :manage, :all
     else
+      can :read, :all
       can :manage, User, id: user.id
       can :manage, Comment, id: user.id
-      cannot :destroy, Comment
     end
   end
 end
