9d1cb724454e2c2ba568e056cd4563c58f3dadb4
heyouli <julia@julia-hofmann.com>
2018-09-25 10:34:37 +0200

    5.11. Add automated mail send

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
            to: 'julia@julia-hofmann.com',
            subject: "A new contact form message from #{name}")
     end
+
+    def welcome(user)
+    @appname = "Fengyue 风雨"
+      mail(to: user.email,
+        subject: "Welcome to #{@appname}!")
+    end
   end
