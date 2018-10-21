dd33140a6d1c5be049bc72b64561088f2a99357e
heyouli <julia@julia-hofmann.com>
2018-10-21 21:46:29 +0200

    6.6 fix orders not accesible

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :comments
 end # Makes comments resources are a subset of products.
 
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
@@ -14,8 +16,7 @@ end # Makes comments resources are a subset of products.
   get 'products/new'
   get 'products/edit'
   get 'products/show'
-  get 'orders/index'
-  get 'orders/show'
+
   post 'simple_pages/thank_you'
   post 'payments/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
