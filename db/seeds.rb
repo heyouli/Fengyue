3a73f79634eb5620bb68902f366b1e53c5a2d1c9
heyouli <julia@julia-hofmann.com>
2018-10-25 13:58:31 +0200

    6.6 fixing order page error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create(name: "seed-generated product", description: "I created this product without using the HTML form!", image_url: "http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=")
+Product.destroy_all
+Order.destroy_all
+product = Product.create(name: "seed-generated product", description: "I created this product without using the HTML form!", image_url: "http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=", price: 22)
+order =Order.create(user_id: 1, product_id: 1, total: 22)
