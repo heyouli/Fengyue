18ad502b4ff2194350b4d1ad9a074f9fd8a020f9
heyouli <julia@julia-hofmann.com>
2018-10-25 14:07:16 +0200

    6.6 fix orders page error 2

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,15 +8,12 @@ class PaymentsController < ApplicationController
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
-      amount: (@product.price * 100).to_i,
-      currency: "eur",
-      source: token,
-      description:@product.name,
-      metadata: {
-        'Email-address:' => params[:stripeEmail]
-      },
-      receipt_email: params[:stripeEmail]
-    )
+       :amount => (@product.price * 100).to_i,
+       :currency => "eur",
+       :source => token,
+       :description => params[:stripeEmail],
+       :receipt_email => params[:stripeEmail]
+     )
 
   if charge.paid
     Order.create(product_id: @product.id,user_id: @user.id, total: @product.price)
