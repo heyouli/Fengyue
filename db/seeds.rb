85e9b1a1e1dc65b30a06c91f44bf4951a62d6973
heyouli <julia@julia-hofmann.com>
2018-11-01 11:05:22 +0100

    6.6 create order in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Comment.destroy_all
-Product.destroy_all
-User.destroy_all
-Order.destroy_all
+
 product = Product.create(name: "seed-generated product", description: "I created this product without using the HTML form!", image_url: "http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=", price: 22)
-# order =Order.create(user_id: 1, product_id: 1, total: 22)
+order =Order.create(user_id: 9, product_id: 9, total: 22)
