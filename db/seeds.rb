2de47ad248e0df9ad5f9975442c98be8abb30776
heyouli <julia@julia-hofmann.com>
2018-11-01 11:20:06 +0100

    6.6 final fix orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 product = Product.create(name: "seed-generated product", description: "I created this product without using the HTML form!", image_url: "http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=", price: 22)
-order =Order.create(user_id: 9, product_id: 9, total: 22)
+order =Order.create(user_id: 9, product_id: 10, total: 22)
