35dc1446ee1bffc79b57b4a7e68815b06291e27c
heyouli <julia@julia-hofmann.com>
2018-11-05 11:46:17 +0100

    6.14 Fixed double comment note

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,21 +4,17 @@ class CommentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
-    @user = request.env['warden'].user
+    @user = current_user
 
     respond_to do |format|
       if @comment.save
         # ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
-        ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user}), average_rating: @product.average_rating
-
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
-
-         format.html { redirect_to @product, notice: 'Review was created successfully.' }
-         format.json { render :show, status: :created, location: @product }
-         format.js
+        format.json { render :show, status: :created, location: @product }
+        format.js
        else
-         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-         format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
        end
      end
    end
